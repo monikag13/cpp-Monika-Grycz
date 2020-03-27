b15e9fd82bf91830f74aa85daff95e9df6a1881b
monikag1307 <53023020+monikag1307@users.noreply.github.com>
2020-03-27 10:48:41 +0100

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+ double a = 0;
+ double b= 0;
+ char c = '3';
+ std::cout << sqrt(-1) << std::endl;
+ std::cout << c/a << std::endl;
+ // inf, -inf , -Nan
+
+return 0;
+}
---
+++ 4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  double x = 1 , y = 20e-65 ;
+  if(x+y == x)
+    {
+     std::cout << "Blad" << std::endl;
+    }
+return 0;
+}
