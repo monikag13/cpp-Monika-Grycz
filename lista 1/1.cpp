d743a22fbc8f8470abf5ba6f941707b9360b7e82
monikag1307 <53023020+monikag1307@users.noreply.github.com>
2020-03-28 16:43:19 +0100

    Update 1.cpp

--- lista 1/1.cpp
+++ lista 1/1.cpp
@@ -9,8 +9,8 @@ int main()
        wynik = wynik * i;
        std::cout << wynik << std::endl;
      }
-/* Zakres liczb int jest ograniczony po jakims przeskakuje na minimalne wartosc ( najpierw ujemne potem na 0 ).
-Mozna wydluzyc zakres dajac zmienne long int. */
+/* Zakres liczb int jest ograniczony (od -32768 do 32767), po pewnym czasie przeskakuje na minimalne wartosci (ujemne, następie 0).
+Mozna wydluzyc zakres dajac zmienną na long int. */
   
 return 0;
 }
