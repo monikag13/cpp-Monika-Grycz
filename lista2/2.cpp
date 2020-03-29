4066259601a27a780889d3c8129e496771258f3b
monikag1307 <53023020+monikag1307@users.noreply.github.com>
2020-03-29 20:29:43 +0200

    Add files via upload

--- lista2/2.cpp
+++ lista2/2.cpp
@@ -8,7 +8,7 @@ void zad2(vector<double> vector) {
 
     {
         fill(vector.begin(), vector.end(), 0);
-        for (int i = 0; i < vector.size(); i++)
+        for (int i = 0; i < vector.size(); ++i)
         {
             cout << vector[i] << " ";
         }
