6b9e0058603d86860eab9e2a47ecf585921fa03d
Monika Grycz <53023020+monikag13@users.noreply.github.com>
2020-04-07 22:18:29 +0200

    Update 2.cpp

--- lista 1/2.cpp
+++ lista 1/2.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <cstdlib>
 #define _USE_MATH_DEFINES
-#include <cmath>
+
 
 using namespace std;
 
 int main()
 {
 	int n = 1000000;
-	double wynik, wynik2, suma = 0, iloczyn = 1, pi = acos(-1), wynik3, suma2 = 0;
-	
+	double skladnik, skladnik2, suma = 0, iloczyn = 1, pi = acos(-1), skladnik3, suma2 = 0;
+
 
 	for (int j = 1; j <= n; ++j)
 	{
-		wynik = (pow(-1, j + 1) / (2.0 * j - 1));
-		suma = suma + wynik;
+		skladnik = (pow(-1, j + 1) / (2.0 * j - 1));
+		suma = suma + skladnik;
 	}
 	cout << "a): " << suma * 4 << endl;
 
 	for (int x = 1; x <= n; x++)
 	{
-		wynik2 = (4.0 * x * x) / ((4.0 * x * x) - 1);
-		iloczyn = (iloczyn * wynik2);
+		skladnik2 = (4.0 * x * x) / ((4.0 * x * x) - 1);
+		iloczyn *= skladnik2;
 	}
 	cout << "b): " << iloczyn * 2 << endl;
 
 
 	for (int y = 1; y <= n; ++y)
 	{
-		wynik3 = 1 / pow(2 * y - 1, 2);
-		suma2 = suma2 + wynik3;
+		skladnik3 = 1 / pow(2 * y - 1, 2);
+		suma2 = suma2 + skladnik3;
 	}
-	cout << "c): " << sqrt(8*suma2) << endl;
+	cout << "c): " << sqrt(8 * suma2) << endl;
 	return 0;
 }
 
