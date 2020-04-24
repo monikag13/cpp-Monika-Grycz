1da670cb3f8ff544324fbc57ace74964bec71ffb
Monika Grycz <53023020+monikag13@users.noreply.github.com>
2020-04-24 12:12:31 +0200

    Add files via upload

---
+++ lista 2/2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void zad2(vector<double> vector) {
+
+    {
+        fill(vector.begin(), vector.end(), 0);
+        for (int i = 0; i < vector.size(); ++i)
+        {
+            cout << vector[i] << " ";
+        }
+    }
+
+
+}
+
+int main()
+{
+    vector<double> v = { 3.5, 2.5, 1.5, 5 };
+    zad2(v);
+    return 0;
+}
