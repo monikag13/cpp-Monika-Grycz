97e76ac8279cf5551331139f377aca751315386f
Monika Grycz <53023020+monikag13@users.noreply.github.com>
2020-04-24 09:45:55 +0200

    Add files via upload

---
+++ lista 4/2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	//2.1
+	double (*w)[100];
+
+	//2.2
+	char *array[5][5];
+
+	//2.3
+	char * array[];
+
+	//2.4
+	vector <void*> v;
+
+	//2.5
+	vector <double> v[4];
+
+	//2.6
+	void (*f)(double tab[], int x);
+
+	//2.7
+	void f(int*& x);
+
+	return 0;
+}
+
