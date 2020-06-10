91033f3989dadc2b93c918904cc3253b8238514e
Monika Grycz <53023020+monikag13@users.noreply.github.com>
2020-06-10 09:23:14 +0200

    Update 2.cpp

--- lista 2/2.cpp
+++ lista 2/2.cpp
@@ -4,22 +4,19 @@
 
 using namespace std;
 
-void zad2(vector<double> vector) {
-
-    {
-        fill(vector.begin(), vector.end(), 0);
-        for (int i = 0; i < vector.size(); ++i)
-        {
-            cout << vector[i] << " ";
-        }
-    }
-
-
+void zad2(vector<double> &vector)
+{
+    fill(vector.begin(), vector.end(), 0);
 }
+
 
 int main()
 {
     vector<double> v = { 3.5, 2.5, 1.5, 5 };
     zad2(v);
+            for (int i = 0; i < v.size(); ++i)
+        {
+            cout << v[i] << " ";
+        }
     return 0;
 }
