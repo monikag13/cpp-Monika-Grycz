5da8dc087cd2246a01da299b7029e4b93fffa87b
monikag13 <53023020+monikag13@users.noreply.github.com>
2020-06-18 18:18:15 +0200

    Add files via upload

---
+++ lista6/zadanie1/stos.cpp
@@ -0,0 +1,85 @@
+#include "stos.h"
+#include <assert.h>
+
+
+Stos::Stos(size_t cap) : 
+	_capacity(cap),
+	_size(0),
+	_tab(new int[cap])
+{
+	assert(cap > 0);
+}
+
+
+void Stos::_grow()
+{
+	assert(_size == _capacity);
+
+	_capacity *= 2;
+	int* p = new int[_capacity];
+	for (size_t i = 0; i < _size; i++)
+	{
+		p[i] = _tab[i];
+	}
+	delete[] _tab;
+	_tab = p;
+}
+
+
+Stos::Stos(Stos const& rhs)
+	: _capacity(rhs._capacity), _size(rhs._size), _tab(new int[_capacity])
+{
+	for (size_t i = 0; i < _size; i++)
+	{
+		_tab[i] = rhs._tab[i];
+	}
+}
+
+
+void Stos::push_noinline(int n)
+{
+	if (_size == _capacity)
+	{
+		_grow();
+	}
+
+	_tab[_size] = n;
+	_size++;
+}
+
+
+void Stos::_shrink(void)  // (void) sygnalizuje brak parametru 
+{
+	assert(_capacity >= 4);
+
+	_capacity /= 2;
+	int* p = new int[_capacity];
+	for (size_t i = 0; i < _size; i++)
+	{
+		p[i] = _tab[i];
+	}
+	delete[] _tab;
+	_tab = p;
+
+};
+
+Stos& Stos::operator=(Stos const& rhs)
+{
+	if (this == &rhs) {
+		return *this;
+	}
+
+	if (_capacity < rhs._size)
+	{
+		delete[] _tab;
+		_capacity = rhs._size;
+		_tab = new int[_capacity];
+	}
+
+	_size = rhs._size;
+	for (size_t i = 0; i < _size; i++)
+	{
+		_tab[i] = rhs._tab[i];
+	}
+	return *this;
+}
---
+++ lista6/zadanie1/stos.h
@@ -0,0 +1,85 @@
+#ifndef __STOS_H__
+#define __STOS_H__
+
+//#pragma once
+
+#include <assert.h>
+#include <iostream>
+
+class Stos
+{
+	public:
+		Stos();
+		Stos(size_t cap);
+		Stos(Stos const&);
+		~Stos();
+
+	void push(int i);
+	void push_noinline(int i);
+	void pop();
+	int top() const;
+	int& top();
+	bool empty() const;
+	size_t size() const;
+	Stos& operator=(Stos const&);
+
+	private:
+		enum { initial_capacity = 1 };
+		void _grow();
+		void _shrink(void);   // zalecenie
+
+	size_t _capacity;
+	size_t _size;
+	int* _tab;
+};
+
+inline Stos::Stos()
+	: _capacity(initial_capacity),
+	_size(0), 
+	_tab(new int[initial_capacity])
+
+{
+	assert(initial_capacity > 0);
+}
+
+inline Stos::~Stos() { delete[] _tab; }
+
+inline void Stos::push(int n)
+{
+	if (_size == _capacity)
+	{
+		_grow();
+	}
+
+	_tab[_size] = n;
+	_size++;
+}
+
+inline void Stos::pop()
+{
+	assert(!empty());
+	_size--;
+
+	if ((_capacity >= 4 * _size) && (_capacity > 4))
+		_shrink();
+}
+
+inline int Stos::top() const
+{
+	assert(!empty());
+	return _tab[_size - 1];
+}
+
+inline int& Stos::top()
+{
+	assert(!empty());
+	return _tab[_size - 1];
+}
+
+inline bool Stos::empty() const { return _size == 0; }
+
+inline size_t Stos::size() const { return _size; }
+
+
+
+#endif // !__STOS_H__
